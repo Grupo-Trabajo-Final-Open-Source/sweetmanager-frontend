1e41a0642e6be3116dd60f0c9a8ffbc6ec02daf9
aaron <U202211552@upc.edu.pe>
2024-05-04 11:48:58 -0500

    feat(monitoring): updated room create and edit ts.

--- src/app/monitoring/rooms/components/room-create-and-edit/room-create-and-edit.component.ts
+++ src/app/monitoring/rooms/components/room-create-and-edit/room-create-and-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {Room} from "../../model/room.entity";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-room-create-and-edit',
@@ -6,5 +8,43 @@ import { Component } from '@angular/core';
   styleUrl: './room-create-and-edit.component.css'
 })
 export class RoomCreateAndEditComponent {
+  // Attributes
+  status: string[] = ['busy','not busy'];
 
+  @Input() room: Room;
+  @Input() editMode = false;
+  @Output() roomAdded = new EventEmitter<Room>();
+  @Output() roomUpdated = new EventEmitter<Room>();
+  @Output() editCanceled = new EventEmitter();
+  @ViewChild('roomForm', {static: false}) roomForm!: NgForm;
+
+  constructor() {
+    this.room = { } as Room;
+  }
+
+  private resetEditState() {
+    this.room = {} as Room;
+    this.editMode = false;
+    this.roomForm.resetForm();
+  }
+
+  onSubmit() {
+    if (this.roomForm.form.valid) {
+
+      let emitter = this.editMode ? this.roomUpdated : this.roomAdded;
+
+      emitter.emit(this.room);
+
+      this.resetEditState();
+    }
+    else {
+      console.error('Invalid data in form');
+    }
+  }
+
+  onCancel() {
+
+    this.editCanceled.emit();
+    this.resetEditState();
+  }
 }
