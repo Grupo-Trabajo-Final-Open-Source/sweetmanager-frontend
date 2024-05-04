ce69e0ad7d7e6437b15549d0a07aef56b096176e
aaron <U202211552@upc.edu.pe>
2024-05-04 11:47:45 -0500

    feat(monitoring): updated room control service.

--- src/app/monitoring/rooms/services/room-control.service.ts
+++ src/app/monitoring/rooms/services/room-control.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BaseService} from "../../shared/services/base.service";
+import {Room} from "../model/room.entity";
 
 @Injectable({
   providedIn: 'root'
 })
-export class RoomControlService {
+export class RoomControlService extends BaseService<Room> {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(http);
+    this.resourceEndpoint = '/rooms';
+  }
 }
