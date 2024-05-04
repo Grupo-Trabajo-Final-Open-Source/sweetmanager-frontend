c1b92818d37e1515c511e2edb25f4bd09e891206
Fernando Salgado <u202212023@upc.edu.pe>
2024-05-04 13:22:43 -0500

    feat(dashboard): reports view implemented

---
+++ src/app/dashboard/charts/pages/reports-view/reports-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-view',
+  templateUrl: './reports-view.component.html',
+  styleUrls: ['./reports-view.component.css']
+})
+export class ReportsViewComponent {}
