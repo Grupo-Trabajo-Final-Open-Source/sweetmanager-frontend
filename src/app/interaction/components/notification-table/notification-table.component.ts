8c663f312845789b6d572ee4ef2dfaf810d33214
Mauricio <mauriciorojasanchez@gmail.com>
2024-05-14 00:26:01 -0500

    feat(interaction): initialized components, models and services for interaction bounded context.

---
+++ src/app/interaction/components/notification-table/notification-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-table',
+  templateUrl: './notification-table.component.html',
+  styleUrl: './notification-table.component.css'
+})
+export class NotificationTableComponent {
+
+}
