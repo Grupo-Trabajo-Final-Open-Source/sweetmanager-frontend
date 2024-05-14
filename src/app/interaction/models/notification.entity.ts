bdaf2e02b7c7cae14599d54690ef89871851d6e7
Mauricio <mauriciorojasanchez@gmail.com>
2024-05-14 00:31:28 -0500

    feat(interaction): implemented notification models, services and components.

--- src/app/interaction/models/notification.entity.ts
+++ src/app/interaction/models/notification.entity.ts
@@ -1,2 +1,17 @@
-export class NotificationEntity {
+export class Notification {
+  id: number;
+
+  title : string;
+
+  message : string;
+
+  date : Date;
+
+  constructor(id = 0, title = '', message = '', date = new Date()) {
+    this.id = id;
+    this.title = title;
+    this.message = message;
+    this.date = date;
+  }
+
 }
--- src/app/interaction/services/notification.service.ts
+++ src/app/interaction/services/notification.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
+  baseUrl: string = 'https://sweetmanager.ryzeon.me';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getNotifications(){
+    return this.http.get(`${this.baseUrl}/notifications`);
+  }
+
 }
