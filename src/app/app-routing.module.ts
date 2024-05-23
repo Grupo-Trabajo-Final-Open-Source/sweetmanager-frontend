7dbefa73e70e698a61281d28dce365f9ad7222d5
Mauricio <mauriciorojasanchez@gmail.com>
2024-05-23 13:41:32 -0500

    feat(feature/routing): implemented routes in app routing modules.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AccessViewComponent} from "./iam/components/access-view/access-view.component";
+import {RoomManagementComponent} from "./monitoring/rooms/pages/room-management/room-management.component";
+import {ReportsViewComponent} from "./dashboard/charts/pages/reports-view/reports-view.component";
+import {SuppliesComponent} from "./supply-management/supply/pages/supplies/supplies.component";
+import {NotificationTableComponent} from "./interaction/components/notification-table/notification-table.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/access-view', pathMatch: 'full'},
+  {path: 'access-view', component: AccessViewComponent},
+  {path: 'monitoring/rooms' , component: RoomManagementComponent},
+  {path: 'dashboard/panel' , component: ReportsViewComponent},
+  {path: 'supply-management/supplies', component: SuppliesComponent},
+  {path: 'interaction/notifications', component: NotificationTableComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
