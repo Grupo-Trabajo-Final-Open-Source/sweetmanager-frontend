9f3d704abbfb3230f5c67427383752db9537f7a6
Mauricio <mauriciorojasanchez@gmail.com>
2024-05-26 01:09:20 -0500

    feat(payment): implemented services and structure in form payment.

--- src/app/payment/components/card-payment/card-payment.component.ts
+++ src/app/payment/components/card-payment/card-payment.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Payment} from "../../models/payment.entity";
+import {UserService} from "../../../iam/services/user.service";
+import {Router} from "@angular/router";
+import {User} from "../../../iam/models/user.entity";
 
 @Component({
   selector: 'app-card-payment',
@@ -7,4 +11,36 @@ import { Component } from '@angular/core';
 })
 export class CardPaymentComponent {
 
+  // Attributes
+  idTitular: number = 0;
+  titularName: string = '';
+  targetAccount: string = '';
+  expirationDate: Date = new Date();
+  cvc: number = 0;
+  email: string = '';
+  users: Array<User> = [];
+
+  constructor(private userService: UserService, private router: Router) {
+  }
+
+  pay(){
+
+    this.userService.getUsers().subscribe((users: any) => {
+      this.users = users;
+
+      let user = this.users.find(u => u.email == this.email);
+
+      if(user != null){
+        this.idTitular = user.id;
+      }
+    })
+
+    let payment = new Payment(this.titularName, this.targetAccount, this.expirationDate, this.cvc, this.email, this.idTitular);
+
+    console.log(payment);
+
+    alert("Pago realizado correctamente!")
+
+    this.router.navigate(['/access-view']);
+  }
 }
