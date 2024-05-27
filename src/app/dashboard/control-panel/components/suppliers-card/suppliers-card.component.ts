e5641188e510a41837766e3ebe4bf1a7cacdb47a
Fernando Salgado <u202212023@upc.edu.pe>
2024-05-27 00:40:02 -0500

    feat(dashboard): suppliers card implemented.

---
+++ src/app/dashboard/control-panel/components/suppliers-card/suppliers-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suppliers-card',
+  templateUrl: './suppliers-card.component.html',
+  styleUrl: './suppliers-card.component.css'
+})
+export class SuppliersCardComponent {
+
+}
